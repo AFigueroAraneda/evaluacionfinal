738520991fb8f8418396452f995197e80460e669
AFigueroaZA <afigueroa@zonaustral.cl>
2025-07-17 12:01:20 -0300

    Add vite-ignore comments to dynamic imports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,19 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'avisos',
-    loadComponent: () => import('./avisos/avisos-list.component').then(m => m.AvisosListComponent)
+    loadComponent: () =>
+      import(
+        /* @vite-ignore */
+        './avisos/avisos-list.component'
+      ).then((m) => m.AvisosListComponent),
   },
   {
     path: 'avisos/nuevo',
-    loadComponent: () => import('./avisos/aviso-form.component').then(m => m.AvisoFormComponent)
+    loadComponent: () =>
+      import(
+        /* @vite-ignore */
+        './avisos/aviso-form.component'
+      ).then((m) => m.AvisoFormComponent),
   },
   {
     path: '',
