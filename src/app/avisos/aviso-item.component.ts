fc1349e3789dbb79bdd5f9c43aa1ac6966ee007c
AFigueroa <afigueroa@zonaustral.cl>
2025-07-21 09:52:12 -0300

    Actualizar dependencias de Angular y Ionic, agregar soporte para localización en español y mejorar formato de código en componentes

--- src/app/avisos/aviso-item.component.ts
+++ src/app/avisos/aviso-item.component.ts
@@ -1,15 +1,14 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonItem, IonLabel, IonButton, IonImg } from '@ionic/angular/standalone';
-import { FechaPipe } from '../pipes/fecha.pipe';
 import { Aviso } from '../services/avisos.service';
 
 @Component({
   selector: 'app-aviso-item',
+  standalone: true,
   templateUrl: './aviso-item.component.html',
   styleUrls: ['./aviso-item.component.scss'],
-  standalone: true,
-  imports: [CommonModule, IonItem, IonLabel, IonButton, IonImg, FechaPipe]
+  imports: [CommonModule, IonItem, IonLabel, IonButton, IonImg],
 })
 export class AvisoItemComponent {
   @Input() aviso!: Aviso;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 /// <reference types="@angular/localize" />
 
 import { bootstrapApplication } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import localeEsCl from '@angular/common/locales/es-CL';
+import { LOCALE_ID } from '@angular/core';
+
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+
 import { defineCustomElements as jeepDefineCustomElements } from 'jeep-sqlite/loader';
 import { defineCustomElements as pwaDefineCustomElements } from '@ionic/pwa-elements/loader';
 import { addIcons } from 'ionicons';
@@ -11,14 +16,21 @@ import { add as addIcon } from 'ionicons/icons';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
-jeepDefineCustomElements(window);
-pwaDefineCustomElements(window);
-addIcons({ add: addIcon });
+// Registrar el locale de Angular
+registerLocaleData(localeEsCl, 'es-CL');
 
+// Bootstrap de la aplicación
 bootstrapApplication(AppComponent, {
   providers: [
+    { provide: LOCALE_ID, useValue: 'es-CL' },
+
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
   ],
 });
+
+// Definir elementos personalizados de Jeep SQLite y PWA Elements
+jeepDefineCustomElements(window);
+pwaDefineCustomElements(window);
+addIcons({ add: addIcon });
